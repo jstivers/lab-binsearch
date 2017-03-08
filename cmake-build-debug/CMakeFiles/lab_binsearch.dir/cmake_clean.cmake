b5253be71152ffb487a7a015552774fb56af6edb
John Stivers <jstivers@usc.edu>
2017-03-07 16:23:43 -0800

    push

---
+++ cmake-build-debug/CMakeFiles/lab_binsearch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_binsearch.dir/binsearch.cpp.o"
+  "lab_binsearch.pdb"
+  "lab_binsearch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_binsearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/lab_binsearch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lab_binsearch.
+# This may be replaced when dependencies are built.
